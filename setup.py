0a0fde996e185755eb69d3c26d83e3fceddeef8a
AV015 <vaghasia.arjun98@gmail.com>
2024-01-20 00:07:29 -0800

    File Updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Arjun Vaghasiya',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
